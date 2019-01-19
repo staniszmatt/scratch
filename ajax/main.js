21fda7922c88c83308d88d266ad8b7eae32b54ae
staniszmatt <matthewpstaniszewski@gmail.com>
2019-01-18 16:21:59 -0800

    added ajax notes

---
+++ ajax/main.js
@@ -0,0 +1,36 @@
+$(document).ready(loadWhenReady);
+
+function loadWhenReady() { 
+    $("selector").addClass("add something"); // basic jQuery 
+
+    //AJAX 
+    // $.ajax({  //$ is a function of an object and ajax() is the method
+    //     type: "method",
+    //     url: "url", 
+    //     data: "data",
+    //     dataType: "dataType",
+    //     success: function (response) {
+    //       //Do something  
+    //     }
+    // });
+console.log("Making ajex fucntions")
+    var ajaxCallOption = {
+        url: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topalbums/limit=25/json",
+        method: "GET",
+        dataType: "json",
+        success: runThisWhenDataComesBack,
+        // error: somefunction
+    }
+    
+    function runThisWhenDataComesBack(response) { 
+        // var responseObject = JSON.parse(response);
+        console.log(response);
+        console.log("success was called");
+     }
+console.log("calling ajax call");
+    $.ajax(ajaxCallOption); 
+    console.log("ending ajax call");
+
+    //core javascript
+    
+}
