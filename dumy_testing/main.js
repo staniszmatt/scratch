983b4270ced72e133e15459cac9c3e2501125d59
staniszmatt <matthewpstaniszewski@gmail.com>
2019-02-23 08:31:49 -0800

    finished a practice problem

--- dumy_testing/main.js
+++ dumy_testing/main.js
@@ -1,33 +1,26 @@
 
-let test = [50,2,56,3,4,7,6,32,66]
+let test = [1,2,5,6]
 
 function makeArrayConsecutive2(statues) {
 	
-   statues.sort(function(a, b){return a-b})
-   console.log("Statues orginized ",statues)
-   const newArray = [];
+   statues.sort((a, b) => {return a-b})
+   let counter = 0;
 
 	const numArrayLength = statues[statues.length-1] - statues[0];
-   let numCheck = statues[0];
-   console.log("Num Check ",numCheck)
    let loopIndex = 0;
    while(loopIndex !== numArrayLength){
    
       let num = statues[loopIndex];
-      console.log("num ",num)
       let numCompare = statues[loopIndex+1];
-      console.log("compare ", numCompare);
 
       if (num + 1 !== numCompare){ 
-      	num += 1
-      	console.log("new Num ", num)
-         statues.splice( statues[loopIndex+1] , 0, num);
-         console.log(statues)
+         num += 1;
+         counter++;
+         statues.splice( loopIndex+1 , 0, num);
       }
       loopIndex++;
-      console.log(setIndex);
    }
-   return statues;
+   return counter;
 }
 
 console.log(makeArrayConsecutive2(test));
