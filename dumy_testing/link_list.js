661cc07b75a981c6d7cfa5c3728d7045d9398902
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-08 10:03:03 -0800

    Completed minimum requirement of a link list. Coming back to add some additional functionality of what a link list could have.

--- dumy_testing/link_list.js
+++ dumy_testing/link_list.js
@@ -1,24 +1,33 @@
+//TODO: Add function to add a node anywhere in the  middle of the node list. 
+//TODO: Add function to delete a node anywhere in between the node head and tail. 
 class LinkedList {
    constructor (){
       this.head = null;
       this.tail = null; 
       this.prev = null;
-  
    }
    add(newValue){
+      //create new object node and add the value passed into it.
+      //The link list is all stored within the specified object that was instantiated. 
       let newNode = {
          value: newValue,
          next: null,
       }
+      //if its the very first created node set tail = to the new node, and then set the head to the tail. 
+      //This sets the head and the tail to the single node. 
       if (this.head === null){
          this.head = this.tail = newNode;
          return;
       }
+      //First line sets the new node prev to the current value of null to show end of tail
+      //Second line links the new tail end node.
+      //The third line sets the new nodes value to the new node at the tail end. 
       newNode.prev = this.tail; 
       this.tail.next = newNode;
       this.tail = newNode;
    }
-   delete(){
+   //This only deletes the last list node.
+   deleteTail(){
       this.tail = this.tail.prev;
       this.tail.next = null; 
    }
