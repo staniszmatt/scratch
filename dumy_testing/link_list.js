c690df944a3fa25d1b389e34dc17e35faabc0708
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-10 18:26:20 -0700

    updated with class notes and updated to my commented link list

--- dumy_testing/link_list.js
+++ dumy_testing/link_list.js
@@ -4,9 +4,9 @@ class LinkedList {
    constructor (){
       this.head = null;
       this.tail = null; 
-      this.prev = null;
+      this.current = null;
    }
-   add(newValue){
+   add(newValue, location = this.tail){
       //create new object node and add the value passed into it.
       //The link list is all stored within the specified object that was instantiated. 
       let newNode = {
@@ -16,20 +16,33 @@ class LinkedList {
       //if its the very first created node set tail = to the new node, and then set the head to the tail. 
       //This sets the head and the tail to the single node. 
       if (this.head === null){
-         this.head = this.tail = newNode;
+         this.current = this.head = this.tail = newNode;
+         return;
+      } else if(location===null){  //Adding node to middle of link list
+         var oldHead = this.head;
+         this.head = newNode;
+         newNode.next = oldHead;
          return;
       }
       //First line sets the new node prev to the current value of null to show end of tail
       //Second line links the new tail end node.
       //The third line sets the new nodes value to the new node at the tail end. 
-      newNode.prev = this.tail; 
-      this.tail.next = newNode;
-      this.tail = newNode;
+      newNode.next = location.next;
+      location.next = newNode;
+      if(location===this.tail){
+         this.tail.next = newNode;
+         this.tail = newNode;
+      }
    }
-   //This only deletes the last list node.
-   deleteTail(){
-      this.tail = this.tail.prev;
-      this.tail.next = null; 
+   search(searchValue){
+      this.current = this.head;
+      while(this.current.value !== searchValue && this.current.next!==null){
+         this.current = this.current.next;
+      }
+      if(this.current.value === searchValue){
+         return this.current;
+      } 
+      return false;
    }
 }
 
@@ -37,5 +50,4 @@ let list = new LinkedList;
 list.add('a');
 list.add('b');
 list.add('c');
-
-console.log(list); 
+console.log("List is: ", list);
