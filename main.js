1f1ed96f9be02c557ac38f44ddf5695a654d241a
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-23 16:38:29 -0700

    Added code from MDN to review and understande to update to my code

--- main.js
+++ main.js
@@ -1,45 +1,74 @@
-// window.onload = startWhenReady;
-
-// function startWhenReady(){
-//   const mouseClick = document.getElementById('click-drag');
-//   mouseClick.addEventListener('mousedown', dragAndDrop);
-//   mouseClick.addEventListener('mouseup', dragAndDrop);
-//   mouseClick.addEventListener('drag', onMouseMove);
-// }
-
-// function dragAndDrop(event){
-//   const wrapper = document.getElementsByClassName('main-div');
-//   console.log(event);
-//   const selectedElm = event.target.parentElement;
+// function handleMouseMove(event){ TODO: Made by myself but running into bubbling issues. 
+//   console.log("handle move, ");
+//   const selectedElm = event.target;
 //   selectedElm.style.position = "absolute";
 
-//   elmToMove.style.left = pageX - elmToMove.offsetWidth / 2 + 'px';
-//   elmToMove.style.top = pageY - elmToMove.offsetHeight / 2 + 'px';
+//   selectedElm.style.left = event.pageX - selectedElm.offsetWidth / 2 + 'px';
+//   selectedElm.style.top = event.pageY - selectedElm.offsetHeight / 2 + 'px';
 // }
 
-// function onMouseMove(event) {
-//   moveAt(event.pageX, event.pageY);
+// function handleMouseDown(event){
+//   console.log("mouse Down, ");
+//   event.target.addEventListener('mousemove', handleMouseMove);
 // }
 
-function handleMouseMove(event){
-  console.log("handle move, ");
-  const selectedElm = event.target;
-  selectedElm.style.position = "absolute";
+// function handleMouseUp(event){
+//   event.target.removeEventListener('mousemove', handleMouseMove);
+//   console.log("mouse UP", event)
+// }
 
-  selectedElm.style.left = event.pageX - selectedElm.offsetWidth / 2 + 'px';
-  selectedElm.style.top = event.pageY - selectedElm.offsetHeight / 2 + 'px';
-}
+// const clickDrag = document.getElementById('click-move');
+// clickDrag.addEventListener('mousedown', handleMouseDown);
+// clickDrag.addEventListener('mouseup', handleMouseUp);
 
-function handleMouseDown(event){
-  console.log("mouse Down, ");
-  event.target.addEventListener('mousemove', handleMouseMove);
-}
+var dragged;
 
-function handleMouseUp(event){
-  event.target.removeEventListener('mousemove', handleMouseMove);
-  console.log("mouse UP", event)
-}
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
 
-const clickDrag = document.getElementById('click-move');
-clickDrag.addEventListener('mousedown', handleMouseDown);
-clickDrag.addEventListener('mouseup', handleMouseUp);
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "purple";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
